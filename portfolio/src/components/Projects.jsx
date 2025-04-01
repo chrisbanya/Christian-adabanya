6fca2fb7e56eb3f498a421b07c73bbbee07bbc31
Adabanya Christian <adabanyachristian21@gmail.com>
2025-04-01 10:11:23 +0100

    refactoring | added new projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -7,22 +7,22 @@ const Projects = () => (
   <>
     <section className="p-5 bg-gray-100 dark:bg-gray-900 pt-24">
       <h2 className="text-4xl font-bold text-center">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 ">
         {projects.map(({ title, image, description, github, link }) => (
           <div
             key={title}
-            className="bg-white dark:bg-gray-800 p-5 rounded-lg shadow transition-transform hover:scale-105"
+            className="flex flex-col bg-white overflow-hidden dark:bg-gray-800 p-5 rounded-lg shadow transition-transform hover:scale-105 max-w-sm"
           >
-            <div className="pb-3">
-              <img
-                src={image}
-                alt={`${title} screenshot`}
-                className="rounded-md object-cover h-40 w-full"
-              />
+            <img
+              src={image}
+              alt={`${title} screenshot`}
+              className="w-full h-40 object-cover  "
+            />
+            <div className="p-4 flex-grow">
+              <h2 className="text-2xl font-semibold text-center">{title}</h2>
+              <p className="mt-2 text-pretty text-justify">{description}</p>
             </div>
-            <h3 className="text-2xl font-semibold text-center">{title}</h3>
-            <p className="mt-2 text-pretty text-justify">{description}</p>
-            <div className="flex gap-3 justify-center mt-2">
+            <div className="p-4 flex gap-4 mx-auto">
               <a href={github} target="_blank" rel="noopener noreferrer ">
                 <button className="flex items-center gap-1 px-5 py-2.5 border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600 rounded text-white text-sm tracking-wider transition-colors shadow-lg">
                   <span>
@@ -45,6 +45,7 @@ const Projects = () => (
         ))}
       </div>
     </section>
+
     <Footer />
   </>
 );
